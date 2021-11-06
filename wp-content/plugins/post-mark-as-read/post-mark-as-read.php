8df931350c85536dde6258086f2016654f8ac29b
alokverma550 <alokverma550@gmail.com>
2021-11-07 02:45:29 +0530

    Plugin main file added.

---
+++ wp-content/plugins/post-mark-as-read/post-mark-as-read.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Post Mark as Read
+* Plugin URI: https://www.algoblend.in/wordpress/plugin/post-mark-as-read/
+* Description: To save user read post data.
+* Version: 1.0
+* Author: Alok Verma
+* Author URI: http://algoblend.com/alok-verma
+**/
